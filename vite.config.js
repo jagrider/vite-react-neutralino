3ccc52eced9091adcf59479f2f681fc5e7987a1e
Jonathan Grider <jgrider2015@gmail.com>
2022-06-26 11:04:29 -0400

    format config files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import Pages from 'vite-plugin-pages'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()]
-})
+  plugins: [react(), Pages()],
+});
