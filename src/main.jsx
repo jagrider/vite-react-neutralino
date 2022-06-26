4b36be100628499ef61dfbd91bc0c6d5fadc66d9
Jonathan Grider <jgrider2015@gmail.com>
2022-06-26 10:45:03 -0400

    added eslint + prettier config

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  BrowserRouter as Router,
-  useRoutes,
-} from 'react-router-dom'
-import routes from '~react-pages'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import routes from '~react-pages';
 
 // File based routing via vite-plugin-pages and React Router
 const Routes = () => {
-  return (
-    <React.Suspense fallback={<p>Loading...</p>}>
-      {useRoutes(routes)}
-    </React.Suspense>
-  )
-}
+  return <React.Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</React.Suspense>;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes />
     </Router>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
 
 if (typeof window !== 'undefined') {
   window.Neutralino.init();
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,47 +1,39 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/logo.svg'
-import '../styles/home.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+import '../styles/home.css';
 
 const Home = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header space-y-3">
         <img alt="logo" className="App-logo" src={logo} />
         <p>Hello Vite + React!</p>
-        <p className='bg-green-300 text-black py-1 px-4 rounded-lg'>
+        <p className="bg-green-300 text-black py-1 px-4 rounded-lg">
           <button onClick={() => setCount((count) => count + 1)} type="button">
             count is: {count}
           </button>
         </p>
-        <Link className='bg-cyan-100 text-black py-1 px-4 rounded-lg' to="/test">Go To Testy</Link>
+        <Link className="bg-cyan-100 text-black py-1 px-4 rounded-lg" to="/test">
+          Go To Testy
+        </Link>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
+          <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
             Learn React
           </a>
           {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
+          <a className="App-link" href="https://vitejs.dev/guide/features.html" rel="noopener noreferrer" target="_blank">
             Vite Docs
           </a>
         </p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
